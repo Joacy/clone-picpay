a0987e9558fac79440929fe3b314b2bfd12f68dd
Joacy <joacymsilva@gmail.com>
2020-08-11 23:09:29 -0300

    Estilização da barra de navegação

--- clone-picpay/src/routes.js
+++ clone-picpay/src/routes.js
@@ -8,16 +8,58 @@ import Pay from './pages/Pay';
 import Notifications from './pages/Notifications';
 import Settings from './pages/Settings';
 
+import PayButton from './components/PayButton';
+
 const Tab = createBottomTabNavigator();
+
+const icons = {
+    Home: {
+        lib: AntDesign,
+        name: 'home',
+    },
+
+    Wallet: {
+        lib: AntDesign,
+        name: 'creditcard',
+    },
+
+    Notifications: {
+        lib: Ionicons,
+        name: 'ios-notifications-outline',
+    },
+
+    Settings: {
+        lib: AntDesign,
+        name: 'setting',
+    },
+};
 
 export default function Routes () {
     return (
         <Tab.Navigator
-            tabBarOptions={{
-                tabStyle: {
-                    justifyContent: "center",
-                    alignItems: "center",
+            initialRouteName="Wallet"
+            screenOptions={({ route, navigation }) => ({
+                tabBarIcon: ({ color, size, focused }) => {
+                    if (route.name === 'Pay') {
+                        return (
+
+                            <PayButton
+                                onPress={() => navigation.navigate('Pay')}
+                                focused={focused}
+                            />);
+                    }
+                    const { lib: Icon, name } = icons[route.name]
+
+                    return <Icon name={name} size={size} color={color} />;
                 },
+            })}
+            tabBarOptions={{
+                style: {
+                    backgroundColor: '#131418',
+                    borderTopColor: 'rgba(255,255,255, 0.5)',
+                },
+                activeTintColor: '#fff',
+                inactiveTintColor: '#92929c',
             }}
         >
             <Tab.Screen
@@ -25,49 +67,38 @@ export default function Routes () {
                 component={Home}
                 options={{
                     title: 'Início',
-                    tabBarIcon: ({ color, size }) => (
-                        <AntDesign name="home" color={color} size={size} />
-                    ),
                 }}
             />
+
             <Tab.Screen
                 name="Wallet"
                 component={Wallet}
                 options={{
                     title: 'Carteira',
-                    tabBarIcon: ({ color, size }) => (
-                        <AntDesign name="creditcard" color={color} size={size} />
-                    ),
                 }}
             />
+
             <Tab.Screen
                 name="Pay"
                 component={Pay}
                 options={{
-                    title: 'Pagar',
-                    tabBarIcon: ({ color, size }) => (
-                        <AntDesign name="creditcard" color={color} size={size} />
-                    ),
+                    title: '',
                 }}
             />
+
             <Tab.Screen
-                name="Notificações"
+                name="Notifications"
                 component={Notifications}
                 options={{
                     title: 'Notificações',
-                    tabBarIcon: ({ color, size }) => (
-                        <Ionicons name="ios-notifications-outline" color={color} size={size} />
-                    ),
                 }}
             />
+
             <Tab.Screen
                 name="Settings"
                 component={Settings}
                 options={{
                     title: 'Ajustes',
-                    tabBarIcon: ({ color, size }) => (
-                        <AntDesign name="setting" color={color} size={size} />
-                    ),
                 }}
             />
         </Tab.Navigator>
